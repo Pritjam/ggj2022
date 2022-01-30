0edb0d3fc9c45e1ac1c4ff6f32e123270e51f2d3
ChrisSmith2 <9710782+ChrisSmith2@users.noreply.github.com>
2022-01-29 19:04:04 -0600

    Add .gitignore

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
 
+    public Text nameText;
+    public Text dialogueText;
     private Queue<string> sentences;
+    private GameObject dialogueBG;
+    private GameObject continueButton;
     // Start is called before the first frame update
     void Start()
     {
         sentences = new Queue<string>();
+        dialogueBG = GameObject.Find("BG");
+        continueButton = GameObject.Find("Continue");
+        dialogueBG.SetActive(false);
+        continueButton.transform.localScale = Vector3.zero;
+
     }
 
     public void StartDialogue(Dialogue d) {
         Debug.Log("Starting conversation with " + d.name);
+        dialogueBG.SetActive(true);
+        continueButton.transform.localScale = Vector3.one;
+        nameText.text = d.name;
+
+        sentences.Clear();
+
+        foreach(string item in d.sentences) {
+            sentences.Enqueue(item);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        if(sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+
+    }
+
+    void EndDialogue() {
+        dialogueText.text = "End of conversation.";
+        dialogueBG.SetActive(false);
+        nameText.text = "";
+        dialogueText.text = "";
+        continueButton.transform.localScale = Vector3.zero;
     }
 }
